648d629d5058fb267e23095f6a9d4903c9fa77fb
Pierre Vincent Hernandez <pierre_hernandez@dlsu.edu.ph>
2022-06-24 18:02:45 +0800

    Add assignment of flip-flop input equations

--- herp3.v
+++ herp3.v
@@ -20,20 +20,29 @@ module JK_FF(Q,J,K,clk,rst);
             end
 endmodule
 
-// Module: Flip-Flop input equations
-module FF_input(JA,KA,JB,KB,JC,KC,JD,KD);
-endmodule
-
+// Module: Up-down BCD Counter
 module UD_BCD_Counter(A,B,C,D,x,clk,rst);
     output A,B,C,D;
     input x,clk,rst;
     wire
-        JA,KA, // A
-        JB,KB, // B
-        JC,KC, // C
-        JD,KD; // D
+        JA,KA, // A JK Flip-Flop
+        JB,KB, // B JK Flip-Flop
+        JC,KC, // C JK Flip-Flop
+        JD,KD; // D JK Flip-Flop
     
     // Assign Flip-Flop input equations
+    // A JK Flip-Flop
+    assign JA = (B & C & D & ~x) | (~B & ~C & ~D & x); // JA = BCDx' + B'C'D'x
+    assign KA = B | C | (D & ~x) | (~D & x); // KA = B + C + Dx' + D'x
+    // B JK Flip-Flop
+    assign JB = (A & ~C & ~D & x) | (~A & C & D & ~x); // JB = AC'D'x + A'CDx'
+    assign KB = A | (C & D & ~x) | (~C & ~D & x); // KB = A + CDx' + C'D'x
+    // C JK Flip-Flop
+    assign JC = (~A & D & ~x) | (A & ~B & ~D & x) | (~A & B & ~D & x); // JC = A'Dx' + AB'D'x + A'BD'x
+    assign KC = (A & C) | (C & D & ~x) | (C & ~D & x); // KC = AC + CDx' + CD'x
+    // D JK Flip-Flop
+    assign JD = ~A | (~B & ~C); // JD = A' + B'C'
+    assign KD = D; // KD = D or KD = 1
 
     // Instantiate JK Flip-Flop
 endmodule
