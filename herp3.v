82889a30d2ab32109ce2242a8d24b7ead99b866d
Pierre Vincent Hernandez <pierre_hernandez@dlsu.edu.ph>
2022-06-26 22:26:07 +0800

    Modify assigned values to flip flop next state

--- herp3.v
+++ herp3.v
@@ -8,13 +8,13 @@ module JK_FF(Q,J,K,clk,rst);
     input clk,rst;
 
     always @(posedge clk,negedge rst)
-        if (rst == 0) Q <= 0;
+        if (rst == 0) Q <= 1'b0;
         else
             begin
                 case ({J,K})
                     2'b00: Q <= Q;
-                    2'b01: Q <= 0;
-                    2'b10: Q <= 1;
+                    2'b01: Q <= 1'b0;
+                    2'b10: Q <= 1'b1;
                     2'b11: Q <= ~Q;
                 endcase 
             end
